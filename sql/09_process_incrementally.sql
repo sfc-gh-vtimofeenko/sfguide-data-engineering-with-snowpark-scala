20e16b7562c1b136ebc22d6ddae0645c62babcdb
Vladimir Timofeenko <vladimir.timofeenko@snowflake.com>
2023-11-29 15:35:49 -0800

    [sql] feat: add step 9 for scala

---
+++ sql/09_process_incrementally.sql
@@ -0,0 +1,76 @@
+/*-----------------------------------------------------------------------------
+Hands-On Lab: Data Engineering with Snowpark
+Script:       09_process_incrementally.sql
+Author:       Jeremiah Hansen, Keith Gaputis, Vladimir Timofeenko
+Last Updated: 11/29/2023
+-----------------------------------------------------------------------------*/
+
+USE ROLE SCALA_DE_ROLE;
+USE WAREHOUSE SCALA_DE_WH;
+USE DATABASE SCALA_DE_DB;
+
+
+-- ----------------------------------------------------------------------------
+-- Step #1: Add new/remaining order data
+-- ----------------------------------------------------------------------------
+
+USE SCHEMA RAW_POS;
+
+ALTER WAREHOUSE SCALA_DE_WH SET WAREHOUSE_SIZE = XLARGE WAIT_FOR_COMPLETION = TRUE;
+
+COPY INTO ORDER_HEADER
+FROM @external.frostbyte_raw_stage/pos/order_header/year=2022
+FILE_FORMAT = (FORMAT_NAME = EXTERNAL.PARQUET_FORMAT)
+MATCH_BY_COLUMN_NAME = CASE_SENSITIVE;
+
+COPY INTO ORDER_DETAIL
+FROM @external.frostbyte_raw_stage/pos/order_detail/year=2022
+FILE_FORMAT = (FORMAT_NAME = EXTERNAL.PARQUET_FORMAT)
+MATCH_BY_COLUMN_NAME = CASE_SENSITIVE;
+
+-- See how many new records are in the stream (this may be a bit slow)
+--SELECT COUNT(*) FROM HARMONIZED.POS_FLATTENED_V_STREAM;
+
+ALTER WAREHOUSE SCALA_DE_WH SET WAREHOUSE_SIZE = XSMALL;
+
+
+-- ----------------------------------------------------------------------------
+-- Step #2: Execute the tasks
+-- ----------------------------------------------------------------------------
+
+USE SCHEMA HARMONIZED;
+
+EXECUTE TASK ORDERS_UPDATE_TASK;
+
+
+-- ----------------------------------------------------------------------------
+-- Step #3: Monitor tasks in Snowsight
+-- ----------------------------------------------------------------------------
+
+/*---
+-- TODO: Add Snowsight details here
+-- https://docs.snowflake.com/en/user-guide/ui-snowsight-tasks.html
+
+-- Remove the filter on "User" and under "Filters" toggle the "Queries executed by user tasks"
+
+
+
+-- Alternatively, here are some manual queries to get at the same details
+SHOW TASKS;
+
+-- Task execution history in the past day
+SELECT *
+FROM TABLE(INFORMATION_SCHEMA.TASK_HISTORY(
+    SCHEDULED_TIME_RANGE_START=>DATEADD('DAY',-1,CURRENT_TIMESTAMP()),
+    RESULT_LIMIT => 100))
+ORDER BY SCHEDULED_TIME DESC
+;
+
+-- Query history in the past hour
+SELECT *
+FROM TABLE(INFORMATION_SCHEMA.QUERY_HISTORY(
+    DATEADD('HOURS',-1,CURRENT_TIMESTAMP()),CURRENT_TIMESTAMP()))
+ORDER BY START_TIME DESC
+LIMIT 100;
+
+---*/
