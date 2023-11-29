1eb9f1407878307691454be9194dd6e0330a0cae
Vladimir Timofeenko <vladimir.timofeenko@snowflake.com>
2023-11-29 11:47:38 -0800

    [scala] feat: implement step 7

---
+++ src/main/scala/com/snowflake/examples/de/Step07_UpdateDailyCityMetricsProcedure.scala
@@ -0,0 +1,199 @@
+package com.snowflake.examples.de
+
+import com.snowflake.examples.utils.TableUtils.tableExists
+import com.snowflake.examples.utils.WithLogging
+import com.snowflake.examples.utils.WithSession
+import com.snowflake.examples.utils.WithWHResize
+import com.snowflake.snowpark.DataFrame
+import com.snowflake.snowpark.Column
+import com.snowflake.snowpark.SaveMode
+import com.snowflake.snowpark.functions._
+import com.snowflake.snowpark.types.StructField
+import com.snowflake.snowpark.types._
+
+import scala.util.Failure
+import scala.util.Success
+import scala.util.Try
+
+object Step07_UpdateDailyCityMetricsProcedure extends WithLogging with WithSession with WithWHResize {
+
+  def createMetricsTable(): Try[Unit] = Try {
+    val schemaForDataFile = StructType(
+      Seq(
+        StructField("DATE", DateType, true),
+        StructField("CITY_NAME", StringType, true),
+        StructField("COUNTRY_DESC", StringType, true),
+        StructField("DAILY_SALES", StringType, true),
+        StructField("AVG_TEMPERATURE_FAHRENHEIT", DecimalType(38, 0)),
+        StructField("AVG_TEMPERATURE_CELSIUS", DecimalType(38, 0)),
+        StructField("AVG_PRECIPITATION_INCHES", DecimalType(38, 0)),
+        StructField("AVG_PRECIPITATION_MILLIMETERS", DecimalType(38, 0)),
+        StructField("MAX_WIND_SPEED_100M_MPH", DecimalType(38, 0)),
+        StructField("META_UPDATED_AT", TimestampType)
+      )
+    )
+
+    val dcm = session createDataFrame (Seq(), schemaForDataFile)
+    dcm.write.mode(SaveMode.Overwrite) saveAsTable ("ANALYTICS.DAILY_CITY_METRICS")
+
+  }
+
+  def mergeDailyCityMetrics(): Try[Unit] = withWHResize {
+
+    /** This allows printing the dataframe while it's being transformed so longer chains of methods don't need to be
+      * interrupted
+      */
+    implicit class PrintableDataframe(df: DataFrame) {
+      def printAndPassDf(dfName: String): DataFrame = {
+        logger info s"$dfName:"
+        df.schema printTreeString
+
+        df
+      }
+    }
+
+    /** Prints the tree structure of the dataframe with a header */
+    def showDataFrame(df: DataFrame, dfName: String): Unit = {
+      logger info s"$dfName:"
+      df.schema printTreeString
+    }
+
+    // Log the count of records in the stream
+    val ordersStreamCount: Long = session table "HARMONIZED.ORDERS_STREAM" count
+
+    logger info (s"There are $ordersStreamCount records in the stream")
+
+    // Get distinct order stream dates
+    val ordersStreamDates: DataFrame =
+      session table "HARMONIZED.ORDERS_STREAM" select col("ORDER_TS_DATE").as("DATE") distinct
+
+    ordersStreamDates limit 5 show
+
+    // Aggregate orders data
+    val orders: DataFrame =
+      session
+        .table("HARMONIZED.ORDER_STREAM")
+        .groupBy(col("ORDER_TS_DATE"), col("PRIMARY_CITY"), col("COUNTRY"))
+        .agg(sum(col("PRICE")).as("price_nulls"))
+        .withColumn("DAILY_SALES", sqlExpr("ZEROIFNULL(price_nulls)"))
+        .select(
+          col("ORDER_TS_DATE").as("DATE"),
+          col("PRIMARY_CITY").as("CITY_NAME"),
+          col("COUNTRY").as("COUNTRY_DESC"),
+          col("DAILY_SALES")
+        )
+
+    // Join weather data with postal codes and countries
+    val weatherPc = session table "FROSTBYTE_WEATHERSOURCE.ONPOINT_ID.POSTAL_CODES" printAndPassDf "weatherPc"
+
+    val countries = session table "RAW_POS.COUNTRY" printAndPassDf "countries"
+
+    val weather = session table "FROSTBYTE_WEATHERSOURCE.ONPOINT_ID.HISTORY_DAY" printAndPassDf "weather"
+
+    val weatherJoined = weather
+      .join(weatherPc, weather col "POSTAL_CODE" equal_to weatherPc.col("POSTAL_CODE"))
+      .join(
+        countries,
+        (weather col "COUNTRY" equal_to countries.col("ISO_COUNTRY"))
+          and
+            (weather col "CITY_NAME" equal_to countries.col("CITY"))
+      )
+      .join(ordersStreamDates, weather col "DATE_VALID_STD" equal_to ordersStreamDates.col("DATE"))
+      .drop(weatherPc col "CITY_NAME")
+      .printAndPassDf("weather after join 1")
+
+    val weatherAgg = weatherJoined
+      .groupBy(col("DATE_VALID_STD"), col("CITY_NAME"), col("COUNTRY"))
+      .agg(
+        avg(col("AVG_TEMPERATURE_AIR_2M_F")).as("AVG_TEMPERATURE_F"),
+        avg(callUDF("HARMONIZED.FAHRENHEIT_TO_CELSIUS_UDF", col("AVG_TEMPERATURE_AIR_2M_F"))).as("AVG_TEMPERATURE_C"),
+        avg(col("TOT_PRECIPITATION_IN")).as("AVG_PRECIPITATION_IN"),
+        avg(callUDF("HARMONIZED.INCH_TO_MILLIMETER_UDF", col("TOT_PRECIPITATION_IN"))).as("AVG_PRECIPITATION_MM"),
+        max(col("MAX_WIND_SPEED_100M_MPH")).as("MAX_WIND_SPEED_100M_MPH")
+      )
+      .select(
+        col("DATE_VALID_STD").as("DATE"),
+        weatherJoined.col("CITY_NAME"),
+        col("COUNTRY").as("COUNTRY_DESC"),
+        round(col("AVG_TEMPERATURE_F"), lit(2)).as("AVG_TEMPERATURE_FAHRENHEIT"),
+        round(col("AVG_TEMPERATURE_C"), lit(2)).as("AVG_TEMPERATURE_CELSIUS"),
+        round(col("AVG_PRECIPITATION_IN"), lit(2)).as("AVG_PRECIPITATION_INCHES"),
+        round(col("AVG_PRECIPITATION_MM"), lit(2)).as("AVG_PRECIPITATION_MILLIMETERS"),
+        col("MAX_WIND_SPEED_100M_MPH")
+      )
+      .printAndPassDf("weather agg")
+
+    // Prepare staging DataFrame for merging
+    val dailyCityMetricsStg: DataFrame = orders
+      .join(
+        weatherAgg,
+        orders col "DATE" equal_to (weatherAgg col "DATE")
+          .and(orders col "CITY_NAME" equal_to weatherAgg.col("CITY_NAME"))
+          .and(orders col "COUNTRY_DESC" equal_to weatherAgg.col("COUNTRY_DESC")),
+        "left"
+      )
+      .drop(weatherAgg.col("DATE"), weatherAgg.col("CITY_NAME"), weatherAgg.col("COUNTRY_DESC"))
+      .printAndPassDf("dailyCityMetricsStg before select")
+      .select(
+        "DATE",
+        "CITY_NAME",
+        "COUNTRY_DESC",
+        "DAILY_SALES",
+        "AVG_TEMPERATURE_FAHRENHEIT",
+        "AVG_TEMPERATURE_CELSIUS",
+        "AVG_PRECIPITATION_INCHES",
+        "AVG_PRECIPITATION_MILLIMETERS",
+        "MAX_WIND_SPEED_100M_MPH"
+      )
+
+    val colsToUpdate: Map[Column, Column] =
+      Map(dailyCityMetricsStg.schema.names map { sourceColName =>
+        (col(sourceColName), dailyCityMetricsStg.col(sourceColName))
+      }: _*) + (col("META_UPDATED_AT") -> current_timestamp())
+
+    val dcm = session table "ANALYTICS.DAILY_CITY_METRICS"
+
+    dcm
+      .merge(
+        dailyCityMetricsStg,
+        // Merge on these columns
+        (Seq("DATE", "CITY_NAME", "COUNTRY_DESC")
+          .map(colname => dcm col colname equal_to dailyCityMetricsStg.col(colname)))
+          .reduce((l, r) => l equal_to r)
+      )
+      .whenMatched
+      .update(colsToUpdate)
+      .whenNotMatched
+      .insert(colsToUpdate)
+      .collect
+  }
+
+  def execute() = {
+    // Create the DAILY_CITY_METRICS table if it doesn't exist
+    if (!tableExists(session, "ANALYTICS", "DAILY_CITY_METRICS")) {
+      createMetricsTable match {
+        case Failure(exception) => {
+          logger error s"Could not create table ANALYTICS.DAILY_CITY_METRICS, reason: $exception"
+          System exit 1
+        }
+        case Success(value) => logger info "Created table ANALYTICS.DAILY_CITY_METRICS"
+      } // TODO: Move to a CreateIfNotExists function?
+    }
+
+    mergeDailyCityMetrics
+
+    "Successfully processed DAILY_CITY_METRICS"
+  }
+
+  /** The main object entrypoint, to be run with sbt "runMain
+    * com.snowflake.examples.de.Step07_UpdateDailyCityMetricsProcedure"
+    *
+    * Modeled after com.snowflake.examples.de.Step07_UpdateDailyCityMetricsProcedure.main in Java quickstart
+    */
+  def main(args: Array[String]): Unit = {
+    val output = execute
+    logger info s"Received output: $output"
+    session close
+  }
+
+}
