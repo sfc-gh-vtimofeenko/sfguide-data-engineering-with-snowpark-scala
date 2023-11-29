e3221406b39ba478bbb0932bd84d1332a202e47b
Vladimir Timofeenko <vladimir.timofeenko@snowflake.com>
2023-11-29 15:34:20 -0800

    [scala] fix: make step 6 deployable

--- src/main/scala/com/snowflake/examples/de/Step06_UpdateOrdersProcedure.scala
+++ src/main/scala/com/snowflake/examples/de/Step06_UpdateOrdersProcedure.scala
@@ -5,6 +5,7 @@ import com.snowflake.examples.utils.WithLogging
 import com.snowflake.examples.utils.WithSession
 import com.snowflake.examples.utils.WithWHResize
 import com.snowflake.snowpark.Column
+import com.snowflake.snowpark.Session
 import com.snowflake.snowpark.functions.col
 import com.snowflake.snowpark.functions.current_timestamp
 
@@ -15,7 +16,7 @@ import scala.util.Try
 object Step06_UpdateOrdersProcedure extends WithLogging with WithSession with WithWHResize {
 
   /** Creates the table called ORDERS */
-  private def createOrdersTable(): Try[Unit] = Try {
+  private def createOrdersTable(session: Session): Try[Unit] = Try {
     logger info "Creating ORDERS table"
     session sql "CREATE TABLE HARMONIZED.ORDERS LIKE HARMONIZED.POS_FLATTENED_V" collect ()
 
@@ -23,13 +24,13 @@ object Step06_UpdateOrdersProcedure extends WithLogging with WithSession with Wi
   }
 
   /** Creates a stream on a the table called ORDERS */
-  private def createOrdersStream(): Try[Unit] = Try {
+  private def createOrdersStream(session: Session): Try[Unit] = Try {
     logger info "Creating ORDERS stream"
     session sql "CREATE STREAM HARMONIZED.ORDERS_STREAM ON TABLE HARMONIZED.ORDERS" collect ()
   }
 
   /** Merges the updates from the stream into HARMONIZED.ORDERS table */
-  private def mergeOrderUpdates(): Try[Unit] = withWHResize {
+  private def mergeOrderUpdates(session: Session): Try[Unit] = withWHResize {
 
     val source = session table "HARMONIZED.POS_FLATTENED_V_STREAM"
     val target = session table "HARMONIZED.ORDERS"
@@ -51,17 +52,19 @@ object Step06_UpdateOrdersProcedure extends WithLogging with WithSession with Wi
   /** Executes the SQL to generate HARMONIZED.ORDERS table
     *
     * Modeled after com.snowflake.examples.de.Step06_UpdateOrdersProcedure.execute in Java quickstart
+    *
+    * It will be used as a stored procedure in Snowflake
     */
-  private def execute(): String = {
+  def execute(session: Session): String = {
     if (!tableExists(session, "HARMONIZED", "ORDERS")) {
-      createOrdersTable match {
+      createOrdersTable(session) match {
         case Failure(exception) => {
           logger error s"Could not create table ORDERS, reason: $exception"
           System exit 1
         }
         case Success(value) => logger info "Created table ORDERS"
       }
-      createOrdersStream match {
+      createOrdersStream(session) match {
         case Failure(exception) => {
           logger error s"Could not create stream ORDERS, reason: $exception"
           System exit 1
@@ -70,7 +73,7 @@ object Step06_UpdateOrdersProcedure extends WithLogging with WithSession with Wi
       }
     }
 
-    mergeOrderUpdates match {
+    mergeOrderUpdates(session) match {
       case Failure(exception) => {
         logger error s"Could not merge updates to ORDERS, reason: $exception"
         System exit 1
@@ -81,12 +84,10 @@ object Step06_UpdateOrdersProcedure extends WithLogging with WithSession with Wi
     "Successfully processed ORDERS"
   }
 
-  /** The main object entrypoint, to be run with sbt "runMain com.snowflake.examples.de.Step06_UpdateOrdersProcedure"
-    *
-    * Modeled after com.snowflake.examples.de.Step06_UpdateOrdersProcedure.main in Java quickstart
-    */
   def main(args: Array[String]): Unit = {
-    val output = execute
+    // NOTE: session here will take the local parameters to execute
+    val output = execute(session)
+
     logger info s"Received output: $output"
     session close
   }
