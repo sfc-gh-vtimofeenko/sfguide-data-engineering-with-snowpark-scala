8b177bc16857fa9b6d8743657fb0a2f1d4ec14a9
Vladimir Timofeenko <vladimir.timofeenko@snowflake.com>
2023-11-29 17:08:41 -0800

    chore: make WithWHResize trait method require session as a parameter
    This allows to pass session at runtime and will help with fixing session
    passing during exectution in Snowpark server-side

--- src/main/scala/com/snowflake/examples/utils/WithWHResize.scala
+++ src/main/scala/com/snowflake/examples/utils/WithWHResize.scala
@@ -3,21 +3,26 @@ package com.snowflake.examples.utils
 import scala.util.Success
 import scala.util.Try
 
-trait WithWHResize extends WithSession {
+trait WithWHResize {
 
   /** Upsizes the warehouse before the warehouse is executed and downsizes it after function is complete
     *
+    * @param session
+    *   a Snowpark session
     * @param f
     *   wrapped function
     * @return
     *   output of f wrapped into [[scala.util.Success]]
     */
-  def withWHResize[T](f: => T): Try[T] = {
+  def withWHResize[T](session: Session, f: => T): T = {
+    // Not using getEnv since we don't want to change the size of a random warehouse
     session sql "ALTER WAREHOUSE SCALA_DE_WH SET WAREHOUSE_SIZE = 'XLARGE' WAIT_FOR_COMPLETION = TRUE" collect ()
+
     val res: T =
       try f
       finally session sql "ALTER WAREHOUSE SCALA_DE_WH SET WAREHOUSE_SIZE = 'XSMALL'" collect ()
-    Success(res)
+
+    res
   }
 
 }
